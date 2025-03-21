22bfe8004695e443c5771eefee2eb81678370939
Thomas Spinec <thomasspinec@device-50.home>
2025-03-21 12:24:11 +0100

    fix: mise à jour de la logique de gestion des candidatures pour afficher correctement l'état de balayage et simplification de l'interface utilisateur

--- front/app/(protected)/recruiter/components/ApplicationsList.tsx
+++ front/app/(protected)/recruiter/components/ApplicationsList.tsx
@@ -55,7 +55,7 @@ export default function ApplicationsList({
 }: ApplicationsListProps) {
   const swiperRef = React.useRef<Swiper<Application>>(null);
   const [index, setIndex] = useState(0);
-  const [isAllSwiped, setIsAllSwiped] = useState(false);
+  const [isAllSwiped, setIsAllSwiped] = useState(applications.length === 0);
 
   const handleSwipe = (direction: "left" | "right", cardIndex: number) => {
     const application = applications[cardIndex];
@@ -70,68 +70,6 @@ export default function ApplicationsList({
   return (
     <View className="flex-1">
       <View className="relative flex-1 ">
-        {/* CANDIDATURES */}
-        <View className="flex-1 pt-16">
-          <Swiper
-            ref={swiperRef}
-            cards={applications}
-            cardIndex={index}
-            renderCard={(application) => (
-              <ApplicationCard application={application} />
-            )}
-            onSwipedLeft={(cardIndex) => handleSwipe("left", cardIndex)}
-            onSwipedRight={(cardIndex) => handleSwipe("right", cardIndex)}
-            onSwipedAll={() => setIsAllSwiped(true)}
-            cardVerticalMargin={50}
-            stackSize={4}
-            stackScale={10}
-            stackSeparation={15}
-            verticalSwipe={false}
-            animateOverlayLabelsOpacity
-            animateCardOpacity
-            backgroundColor="transparent"
-            overlayLabels={{
-              left: {
-                element: <OverlayLabel color="red" />,
-                title: "REFUSER",
-                style: {
-                  label: {
-                    backgroundColor: "black",
-                    borderColor: "black",
-                    color: "white",
-                    borderWidth: 1,
-                  },
-                  wrapper: {
-                    flexDirection: "column",
-                    alignItems: "flex-end",
-                    justifyContent: "flex-start",
-                    marginTop: 30,
-                    marginLeft: -30,
-                  },
-                },
-              },
-              right: {
-                element: <OverlayLabel color="green" />,
-                title: "ACCEPTER",
-                style: {
-                  label: {
-                    backgroundColor: "black",
-                    borderColor: "black",
-                    color: "white",
-                    borderWidth: 1,
-                  },
-                  wrapper: {
-                    flexDirection: "column",
-                    alignItems: "flex-start",
-                    justifyContent: "flex-start",
-                    marginTop: 30,
-                    marginLeft: 30,
-                  },
-                },
-              },
-            }}
-          />
-        </View>
         {/* Retour à la liste des offres */}
         <View className="absolute top-0 left-0 right-0 z-[51]">
           <TouchableOpacity
@@ -145,39 +83,110 @@ export default function ApplicationsList({
           </TouchableOpacity>
         </View>
 
-        {/* Boutons pour accepter ou refuser une candidature */}
-        <View className="absolute bottom-0 left-0 right-0 z-50">
-          <View className="flex-row items-center w-full p-4 justify-evenly">
-            <TouchableOpacity onPress={() => swiperRef.current?.swipeLeft()}>
-              <X size={28} color="red" />
-            </TouchableOpacity>
-            <Text className="text-gray-600">
-              Swipez les cartes pour les classer
-            </Text>
-            <TouchableOpacity onPress={() => swiperRef.current?.swipeRight()}>
-              <Check size={28} color="green" />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+        {!isAllSwiped ? (
+          <>
+            {/* CANDIDATURES */}
+            <View className="flex-1 pt-16">
+              <Swiper
+                ref={swiperRef}
+                cards={applications}
+                cardIndex={index}
+                renderCard={(application) => (
+                  <ApplicationCard application={application} />
+                )}
+                onSwipedLeft={(cardIndex) => handleSwipe("left", cardIndex)}
+                onSwipedRight={(cardIndex) => handleSwipe("right", cardIndex)}
+                onSwipedAll={() => setIsAllSwiped(true)}
+                cardVerticalMargin={50}
+                stackSize={4}
+                stackScale={10}
+                stackSeparation={15}
+                verticalSwipe={false}
+                animateOverlayLabelsOpacity
+                animateCardOpacity
+                backgroundColor="transparent"
+                overlayLabels={{
+                  left: {
+                    element: <OverlayLabel color="red" />,
+                    title: "REFUSER",
+                    style: {
+                      label: {
+                        backgroundColor: "black",
+                        borderColor: "black",
+                        color: "white",
+                        borderWidth: 1,
+                      },
+                      wrapper: {
+                        flexDirection: "column",
+                        alignItems: "flex-end",
+                        justifyContent: "flex-start",
+                        marginTop: 30,
+                        marginLeft: -30,
+                      },
+                    },
+                  },
+                  right: {
+                    element: <OverlayLabel color="green" />,
+                    title: "ACCEPTER",
+                    style: {
+                      label: {
+                        backgroundColor: "black",
+                        borderColor: "black",
+                        color: "white",
+                        borderWidth: 1,
+                      },
+                      wrapper: {
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                        justifyContent: "flex-start",
+                        marginTop: 30,
+                        marginLeft: 30,
+                      },
+                    },
+                  },
+                }}
+              />
+            </View>
 
-      {isAllSwiped && (
-        <View className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full bg-black z-[50]">
-          <Text className="mb-4 text-center text-white">
-            Plus aucune candidature à traiter
-          </Text>
-          <TouchableOpacity
-            className="px-6 py-3 bg-white rounded-lg"
-            onPress={() => {
-              setIsAllSwiped(false);
-              setIndex(0);
-              swiperRef.current?.jumpToCardIndex(0);
-            }}
-          >
-            <Text className="font-medium text-black">Recommencer</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+            {/* Boutons pour accepter ou refuser une candidature */}
+            <View className="absolute bottom-0 left-0 right-0 z-50">
+              <View className="flex-row items-center w-full p-4 justify-evenly">
+                <TouchableOpacity
+                  onPress={() => swiperRef.current?.swipeLeft()}
+                >
+                  <X size={28} color="red" />
+                </TouchableOpacity>
+                <Text className="text-gray-600">
+                  Swipez les cartes pour les classer
+                </Text>
+                <TouchableOpacity
+                  onPress={() => swiperRef.current?.swipeRight()}
+                >
+                  <Check size={28} color="green" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </>
+        ) : (
+          <View className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full bg-black z-[50]">
+            <Text className="mb-4 text-center text-white">
+              Plus aucune candidature à traiter
+            </Text>
+            {applications.length > 0 && (
+              <TouchableOpacity
+                className="px-6 py-3 bg-white rounded-lg"
+                onPress={() => {
+                  setIsAllSwiped(false);
+                  setIndex(0);
+                  swiperRef.current?.jumpToCardIndex(0);
+                }}
+              >
+                <Text className="font-medium text-black">Recommencer</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        )}
+      </View>
     </View>
   );
 }
