e9cdfcff2530f6b6608a6286676d6384f7de564d
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2025-03-21 13:44:43 +0100

    feat: refactor types and add QueryParams struct for request handling

--- back/pkg/skillly/types.go
+++
@@ -1,7 +0,0 @@
-package skillly
-
-// RoleType defines the available roles for a user
-type RoleType string
-type ContractType string
-type CompanyRole string
-type RecruiterState string
---
+++ back/pkg/types/types.go
@@ -0,0 +1,16 @@
+package types
+
+// RoleType defines the available roles for a user
+type RoleType string
+type ContractType string
+type CompanyRole string
+type RecruiterState string
+
+type QueryParams struct {
+	Page     int
+	PageSize *int
+	Sort     string
+	Order    string
+	Populate []string
+	Filters  map[string]string
+}
