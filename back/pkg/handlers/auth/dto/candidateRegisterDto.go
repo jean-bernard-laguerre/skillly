2b33cf8bb965f93f29ad8fa38cfe0e3ff9a1ab03
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2025-04-22 10:40:35 +0200

    refactor: update CandidateRegisterDTO and CreateCandidateDTO to use uint for certifications and skills

--- back/pkg/handlers/auth/dto/candidateRegisterDto.go
+++ back/pkg/handlers/auth/dto/candidateRegisterDto.go
@@ -1,22 +1,22 @@
 package authDto
 
 import (
-	"skillly/pkg/models"
 	"skillly/pkg/utils"
 )
 
 type CandidateRegisterDTO struct {
-	FirstName        string                 `json:"firstName" binding:"required"`
-	LastName         string                 `json:"lastName" binding:"required"`
-	Email            string                 `json:"email" binding:"required"`
-	Password         string                 `json:"password" binding:"required"`
-	Bio              string                 `json:"bio"`
-	ExperienceYear   int                    `json:"experienceYears"`
-	PreferedContract utils.ContractType     `json:"preferedContract"`
-	PreferedJob      string                 `json:"preferedJob"`
-	Location         string                 `json:"location"`
-	Availability     string                 `json:"availability"`
-	ResumeID         uint                   `json:"resumeID"`
-	Certifications   []models.Certification `json:"certifications"`
-	Skills           []models.Skill         `json:"skills"`
+	FirstName        string             `json:"firstName" binding:"required"`
+	LastName         string             `json:"lastName" binding:"required"`
+	Email            string             `json:"email" binding:"required"`
+	Password         string             `json:"password" binding:"required"`
+	Bio              string             `json:"bio"`
+	ExperienceYear   int                `json:"experienceYears"`
+	PreferedContract utils.ContractType `json:"preferedContract"`
+	PreferedJob      string             `json:"preferedJob"`
+	Location         string             `json:"location"`
+	Availability     string             `json:"availability"`
+	ResumeID         uint               `json:"resumeID"`
+
+	Certifications []uint `json:"certifications"`
+	Skills         []uint `json:"skills"`
 }
