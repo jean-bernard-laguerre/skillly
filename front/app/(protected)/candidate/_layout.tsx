7e5877996b03ec44b3d9ea09267e31c9cef90581
Thomas Spinec <thomasspinec@device-50.home>
2025-05-30 14:50:22 +0200

    refactor: simplification de la navigation dans le layout des candidats
    - Suppression de la prop 'role' dans le composant TabNavigator pour une gestion plus directe.
    - Décommentaire et réintroduction du composant CandidateLayout avec une structure de navigation complète pour les candidats.

--- front/app/(protected)/candidate/_layout.tsx
+++ front/app/(protected)/candidate/_layout.tsx
@@ -1,11 +1,11 @@
-// import { Tabs } from "expo-router";
+import { Tabs } from "expo-router";
 
-// export default function CandidateLayout() {
-//   return (
-//     <Tabs screenOptions={{ headerShown: false }}>
-//       <Tabs.Screen name="index" options={{ title: "Accueil" }} />
-//       <Tabs.Screen name="dashboard" options={{ title: "Espace Candidat" }} />
-//       <Tabs.Screen name="jobOffers" options={{ title: "Offres d'emploi" }} />
-//     </Tabs>
-//   );
-// }
+export default function CandidateLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ title: "Accueil" }} />
+      <Tabs.Screen name="dashboard" options={{ title: "Espace Candidat" }} />
+      <Tabs.Screen name="jobOffers" options={{ title: "Offres d'emploi" }} />
+    </Tabs>
+  );
+}
