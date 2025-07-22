6c2762e6381ed41ee547928e142bbb4325d6f2f4
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2025-07-23 01:03:24 +0200

    feat(android): add Detox testing support and update build configurations

---
+++ front/android/app/src/androidTest/java/com/anonymous/skillly/DetoxTest.java
@@ -0,0 +1,21 @@
+package com.anonymous.skillly;
+
+import com.wix.detox.Detox;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+@RunWith(AndroidJUnit4.class)
+public class DetoxTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void runDetoxTests() {
+        Detox.runTests(mActivityRule);
+    }
+}
