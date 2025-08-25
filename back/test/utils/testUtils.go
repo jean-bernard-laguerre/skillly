4ce01679c6d9eff42fcb2fba84fc5a1fbfb509bb
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2025-08-25 14:25:59 +0200

    fix(test): update password for TestRecruiter and TestCandidate to match validation requirements

--- back/test/utils/testUtils.go
+++ back/test/utils/testUtils.go
@@ -75,7 +75,7 @@ var TestRecruiter = authDto.RecruterRegisterDTO{
 	FirstName: "Test",
 	LastName:  "Recruiter",
 	Email:     "TestRecruiter@test.com",
-	Password:  "password123",
+	Password:  "Password123!",
 	Title:     "Test Title",
 	NewCompany: &companyDto.CreateCompanyDTO{
 		CompanyName: "Test Company",
@@ -93,7 +93,7 @@ var TestCandidate = authDto.CandidateRegisterDTO{
 	FirstName:        "Test",
 	LastName:         "Candidate",
 	Email:            "TestCandidate@test.com",
-	Password:         "password123",
+	Password:         "Password123!",
 	Bio:              "Test Bio",
 	ExperienceYear:   5,
 	PreferedContract: models.CDIContract,
