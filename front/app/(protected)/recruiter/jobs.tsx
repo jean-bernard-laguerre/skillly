4f2e4574e7ad5ff6f174aed0352b732dc3513305
Thomas Spinec <thomas.spinec@laplateforme.io>
2025-08-27 09:08:38 +0200

    fix(recruiter): ajuster la position du style dans jobs.tsx et simplifier le rendu dans ScreenWrapper.tsx

--- front/app/(protected)/recruiter/jobs.tsx
+++ front/app/(protected)/recruiter/jobs.tsx
@@ -725,7 +725,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    top: 0,
+    top: 50,
     backgroundColor: "rgba(0, 0, 0, 0.5)",
     justifyContent: "center",
     alignItems: "center",
--- front/navigation/ScreenWrapper.tsx
+++ front/navigation/ScreenWrapper.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useNavigationVisibility } from "@/context/NavigationVisibilityContext";
 
 interface ScreenWrapperProps {
   children: React.ReactNode;
@@ -9,16 +8,8 @@ interface ScreenWrapperProps {
 }
 
 export default function ScreenWrapper({ children, style }: ScreenWrapperProps) {
-  const { isNavigationVisible } = useNavigationVisibility();
-
   return (
-    <SafeAreaView
-      style={[
-        {flex: 1},
-        style,
-      ]}
-       edges={["top"]}
-    >
+    <SafeAreaView style={[{ flex: 1 }, style]} edges={["top"]}>
       {children}
     </SafeAreaView>
   );
